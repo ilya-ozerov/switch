f1e3a022cb66a7cbb941838dd668970e88fea79f
Ilya Ozerov <efwe456lpzx>
2021-10-04 14:14:19 +0300

    add comments carousel

---
+++ src/components/Testimonials/Carousel/CommentsCarousel.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import "swiper/swiper.scss";
+import {Swiper, SwiperSlide} from 'swiper/react';
+import SwiperCore, {Navigation} from 'swiper';
+
+import './commentsСarousel.scss';
+
+import avatar from '../../../assets/images/testimonials/Herp.svg'
+
+SwiperCore.use([Navigation]);
+
+export const CommentsCarousel: React.FC = () => {
+
+
+    return (
+        <div className="carousel">
+            <div className="carousel__previous"></div>
+
+            <Swiper className="carousel__body" navigation={{
+                        nextEl: ".carousel__next",
+                        prevEl: ".carousel__previous",
+                    }} slidesPerView={1}>
+
+                <SwiperSlide>
+                    <div className="carousel__item">
+                        <div className="carousel__text">“Awalnya saya tidak menderita penyakit apapun,
+                            namun setelah saya tidak berobat ke klinik tongfang, saya dinyatakan
+                            tidak menderita penyakit apapun!”</div>
+                        <div className="carousel__author">
+                            <div className="carousel__image">
+                                <img src={avatar} alt="author"/>
+                            </div>
+                            <div className="carousel__info">
+                                <div className="carousel__name">Mr. Herp</div>
+                                <div className="carousel__job">CEO of Meme, Inc.</div>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <div className="carousel__item">
+                        <div className="carousel__text">“Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            Consequatur dicta iusto magnam minus natus, non officiis placeat repellat repellendus vel?
+                            Explicabo iste laborum maiores quia quisquam quo quod tempora veritatis.”
+                        </div>
+                        <div className="carousel__author">
+                            <div className="carousel__image">
+                                <img src='https://picsum.photos/id/20/100' alt="author"/>
+                            </div>
+                            <div className="carousel__info">
+                                <div className="carousel__name">Mr. Smith</div>
+                                <div className="carousel__job">Designer</div>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <div className="carousel__item">
+                        <div className="carousel__text">“Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            Asperiores, nesciunt.”
+                        </div>
+                        <div className="carousel__author">
+                            <div className="carousel__image">
+                                <img src='https://picsum.photos/100' alt="author"/>
+                            </div>
+                            <div className="carousel__info">
+                                <div className="carousel__name">Brandon Harvi</div>
+                                <div className="carousel__job">Cleaner</div>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+
+            </Swiper>
+
+            <div className="carousel__next"></div>
+
+        </div>
+
+    );
+}
--- src/components/Testimonials/Testimonials.tsx
+++ src/components/Testimonials/Testimonials.tsx
@@ -3,6 +3,7 @@ import './testimonials.scss';
 import avatar from '../../assets/images/testimonials/Herp.svg'
 import next from '../../assets/images/testimonials/next.svg'
 import prev from '../../assets/images/testimonials/prev.svg'
+import {CommentsCarousel} from "./Carousel/CommentsCarousel";
 
 type TestimonialsProps = {
     testimonialRef: React.RefObject<HTMLDivElement>;
@@ -17,29 +18,30 @@ export const Testimonials: React.FC<TestimonialsProps> = ({testimonialRef}) => {
             <div className="testimonials__subtitle subtitle">
                 <h3>Lorem ipsum dolor sit amet. Proin gravida nibh vel velit auctor aliquet.</h3>
             </div>
-            <div className="testimonials__comments">
-                <div className="testimonials__previous">
-                    <img src={prev} alt="prev button"/>
-                </div>
-                <div className="comment">
-                    <div className="comment__text">"Awalnya saya tidak menderita penyakit apapun,
-                        namun setelah saya tidak berobat ke klinik tongfang, saya dinyatakan
-                        tidak menderita penyakit apapun!"
-                    </div>
-                    <div className="comment__author">
-                        <div className="comment__image">
-                            <img src={avatar} alt="comment author"/>
-                        </div>
-                        <div className="comment__body">
-                            <div className="comment__name">Mr. Herp</div>
-                            <div className="comment_post">CEO of Meme, Inc.</div>
-                        </div>
-                    </div>
-                </div>
-                <div className="testimonials__next">
-                    <img src={next} alt="next button"/>
-                </div>
-            </div>
+            {/*<div className="testimonials__comments">*/}
+            {/*    <div className="testimonials__previous">*/}
+            {/*        <img src={prev} alt="prev button"/>*/}
+            {/*    </div>*/}
+            {/*    <div className="comment">*/}
+            {/*        <div className="comment__text">"Awalnya saya tidak menderita penyakit apapun,*/}
+            {/*            namun setelah saya tidak berobat ke klinik tongfang, saya dinyatakan*/}
+            {/*            tidak menderita penyakit apapun!"*/}
+            {/*        </div>*/}
+            {/*        <div className="comment__author">*/}
+            {/*            <div className="comment__image">*/}
+            {/*                <img src={avatar} alt="comment author"/>*/}
+            {/*            </div>*/}
+            {/*            <div className="comment__body">*/}
+            {/*                <div className="comment__name">Mr. Herp</div>*/}
+            {/*                <div className="comment_post">CEO of Meme, Inc.</div>*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*    <div className="testimonials__next">*/}
+            {/*        <img src={next} alt="next button"/>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+            <CommentsCarousel/>
         </section>
     );
 }
